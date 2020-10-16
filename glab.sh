45c0ff852bb0a9770c5abace0affcb2cd5befaf3
bratekarate <bratekannkarate@gmail.com>
2020-10-16 20:19:09 +0200

    improve token handling

--- glab.sh
+++ glab.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
-[ "$#" -lt 1 ] &&
-  {
-    echo "Error: missing arguments" >&2
-    exit 1
-  }
+error() {
+	echo "Error: $1" >&2
+	exit 1
+}
 
+[ "$#" -lt 1 ] &&
+	error 'missing arguments.'
+
+TOKEN=${TOKEN:-$(eval "$TOKEN_CMD")}
 URI=$1
 shift
 
-curl -H "$TOKEN" "$BASEURL/api/v4/$URI" | 
-  jq "$@" --raw-output \
-  'if type == "array" then to_entries | [ .[] | {n: .key} + .value ] else . end'
+# shellcheck disable=SC2016
+[ -z "$TOKEN" ] || [ -z "$BASEURL" ] &&
+	error 'env variables $BASEURL and either $TOKEN or $TOKEN_CMD must be set.'
+
+curl -H "PRIVATE-TOKEN: $TOKEN" "$BASEURL/api/v4/$URI" |
+	jq "$@" --raw-output \
+		'if type == "array" then to_entries | [ .[] | {n: .key} + .value ] else . end'
