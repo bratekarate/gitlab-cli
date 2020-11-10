a951031a83a0521259a8af75043a5c33ff17dfc4
bratekarate <bratekannkarate@gmail.com>
2020-11-10 17:28:59 +0100

    fix vim diff

--- glmergediff.sh
+++ glmergediff.sh
@@ -4,5 +4,5 @@ JQDIFF='.changes | .[] | "diff --git a/glsimp.sh b/glsimp2.sh\n--- '"\
 "'\(.old_path)\n+++ \(.new_path)\n\(.diff)"'
 
 JSON=$(glab projects/"$1"/merge_requests/"$2"/changes) &&
-  echo "$JSON" | jq -e 'length > 0' > /dev/null &&
-	echo "$JSON" | jq --compact-output --raw-output "$JQDIFF"
+  printf '%s\n' "$JSON" | jq -e 'length > 0' > /dev/null &&
+	printf '%s\n' "$JSON" | jq --compact-output --raw-output "$JQDIFF"
