52fbe682d2cabb48c02f6a3d37adaecad3740337
bratekarate <bratekannkarate@gmail.com>
2020-11-12 10:55:25 +0100

    improve MR review flow

--- bin/glmergerev.sh
+++ bin/glmergerev.sh
@@ -1,17 +1,39 @@
 #!/bin/sh
 
+ACCEPT_MERGE=0
+while getopts ":abcdefghijklmnopqrstuvwxyz" O; do
+	case "$O" in
+	m)
+		ACCEPT_MERGE=1
+		;;
+	*)
+		echo "Error: unknown option -$O." >&2
+		exit 1
+		;;
+	esac
+done
+shift $((OPTIND - 1))
+
 [ "$#" -lt 2 ] &&
   {
     echo "Error: missing arguments" >&2
     exit 1
   }
 
-glmergediff "$1" "$2" > /tmp/glmerge.diff &&
-  vim /tmp/glmerge.diff < /dev/tty &&
+export PRPATH=$(glab projects/"$1" | jq --raw-output '.path_with_namespace' | sed 's|/|_|g')
+export TITLE=$(glab projects/"$1"/merge_requests/"$2"| jq --raw-output '.title' | sed 's/ /_/g')
+
+FILENAME="/tmp/${PRPATH}_$TITLE" 
+glmergediff "$1" "$2" > "$FILENAME" &&
+    {
+      vim "$FILENAME" +'w' < /dev/tty
+      rm "$FILENAME"
+    } &&
+  [ "$ACCEPT_MERGE" -eq 1 ] &&
   P='y\(es\)\?' &&
   while ! echo "$A" | grep -iq "^\($P\|no\?\)$"; do
     printf 'Accept merge request? [y/n] ' &&
     read -r A
   done < /dev/tty &&
   echo "$A" |  grep -iq "^$P$" &&
-  glmergeacc 449 1
+  glmergeacc "$1" "$2"
