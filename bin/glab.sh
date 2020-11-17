b5f10d982985eaf9c3dbb77b313641500b42ef22
Felix Jian <f.jian@it-frankfurt.com>
2020-11-17 14:44:23 +0100

    always use content type application/json

--- bin/glab.sh
+++ bin/glab.sh
@@ -25,7 +25,13 @@ shift
 # TODO: fifo does not work on MSYS/CYGWIN. curl `-o` issue?
 # mkfifo /tmp/curlout_$$.json
 
-CODE=$(curl -s "$@" -H "PRIVATE-TOKEN: $TOKEN" "$BASEURL/api/v4/$URI" -o /tmp/curlout_$$.json -w "%{http_code}")
+CODE=$(curl -Ss "$@" \
+    -H "Private-Token: $TOKEN" \
+    -H 'Content-Type: application/json' \
+    -o /tmp/curlout_$$.json -w '%{http_code}' \
+    "$BASEURL/api/v4/$URI")
+
+echo "HTTP Code: $CODE" >&2
 
 jq --raw-output \
   'if type == "array" then to_entries | [ .[] | {n: .key} + .value ] else . end' /tmp/curlout_$$.json
