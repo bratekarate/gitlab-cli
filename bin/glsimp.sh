7712538db44fdb4629ee31a8a07a12be11f79308
Felix Jian <f.jian@it-frankfurt.com>
2020-11-20 15:28:49 +0100

    tailor glsimp for usage with projects (most common use case) and add TODO to make it more versatile in the future

--- bin/glsimp.sh
+++ bin/glsimp.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-jq "$@" 'def form: if has("n") then {n} else null end + {id,name,web_url}; if type == "array" then [ .[] | form ] else form end'
+# TODO: currently only usasble for project. use getopts to specify what type of
+# object to simplify and use project as default.
+
+jq "$@" 'def form: if has("n") then {n} else null end + {id,name,path_with_namespace,web_url,ssh_url_to_repo}; if type == "array" then [ .[] | form ] else form end'
