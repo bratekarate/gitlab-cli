1d1a4675ae9212d41da106d74cf2bebedf626e26
bratekarate <bratekannkarate@gmail.com>
2021-09-04 16:17:29 +0200

    optimize glsearch pagination

--- bin/glsearch_ext.sh
+++ bin/glsearch_ext.sh
@@ -1,27 +1,41 @@
 #!/bin/sh
 
+handle_key() {
+  HK_HEAD=$(cat)
+  if GRP=$(printf '%s\n' "$HK_HEAD" | grep "rel=\"$1\""); then
+    printf '%s\n' "$GRP" | cut -d ';' -f1
+  else
+    printf '%s\n' "$HK_HEAD" | grep "rel=\"$2\"" | cut -d ';' -f1
+  fi
+}
+
 CODE=
 RAW=$(glsearch_pages "$@")
 while [ -z "$CODE" ] || { [ "$CODE" -ne 0 ] && [ "$CODE" -ne 1 ] && [ "$CODE" -ne 2 ]; }; do
-  HEAD=$(printf '%s\n' "$RAW" | sed '$ d' | awk -F ': ' '/Link:/{print $2}' | tr ',' '\n' | tr -d '<' | tr -d '>' | tr -d ' ')
-  RES=$(printf '%s\n' "$RAW" | tail -n 1 | jq -r 'if type == "array" then to_entries | [ .[] | {n: .key} + .value ] else . end')
+  HEAD=$(
+    printf '%s\n' "$RAW" |
+      sed '$ d' |
+      awk -F ': ' '/Link:/{print $2}' |
+      tr ',' '\n' |
+      tr -d '<' |
+      tr -d '>' |
+      tr -d ' '
+  )
+  RES=$(
+    printf '%s\n' "$RAW" |
+      tail -n 1 |
+      jq -r 'if type == "array" then to_entries | [ .[] | {n: .key} + .value ] else . end'
+  )
+
+  # TODO: How to customize glpick command?
   printf '%s\n' "$RES" | glpick
   CODE=$?
 
-  [ "$CODE" -eq 10 ] &&
-    if GRP=$(printf '%s\n' "$HEAD" | grep 'rel="next"'); then
-      URL=$(printf '%s\n' "$GRP" | cut -d ';' -f1)
-    else
-      URL=$(printf '%s\n' "$HEAD" | grep 'rel="last"' | cut -d ';' -f1)
-    fi
+  case "$CODE" in
+  0|1|2) exit "$CODE" ;;
+  10) URL=$(printf '%s\n' "$HEAD" | handle_key next last) ;;
+  11) URL=$(printf '%s\n' "$HEAD" | handle_key prev first) ;;
+  esac
 
-  [ "$CODE" -eq 11 ] &&
-    if GRP=$(printf '%s\n' "$HEAD" | grep 'rel="prev"'); then
-      URL=$(printf '%s\n' "$GRP" | cut -d ';' -f1)
-    else
-      URL=$(printf '%s\n' "$HEAD" | grep 'rel="first"' | cut -d ';' -f1)
-    fi
-
-  URL=$(printf '%s\n' "$URL" | sed 's|.*/v4\(/.*\)|\1|g')
-  RAW=$(glab "$URL")
+  RAW=$(glab "$(printf '%s\n' "$URL" | sed 's|.*/v4\(/.*\)|\1|g')")
 done
